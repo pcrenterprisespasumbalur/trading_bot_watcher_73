6949de372534dd3646abcbb6ed028383bc21d4ec
pcrenterprisespasumbalur <pcrenterprisespasumbalur@gmail.com>
2025-03-08 15:46:56 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,5 +3,9 @@ from core.binance_api import binance
 try:
     market_data = binance.fetch_ticker("BTC/USDT")
     print("✅ Market Data:", market_data)
+except ccxt.NetworkError as e:
+        print(f"⚠️ Network error: {e}")
+except ccxt.ExchangeError as e:
+        print(f"⚠️ Binance API error: {e}")
 except Exception as e:
     print("❌ Binance API Error:", e)
