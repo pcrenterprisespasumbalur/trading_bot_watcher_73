80389c44e7928aea5f68b4275eb331508fdde55d
pcrenterprisespasumbalur <pcrenterprisespasumbalur@gmail.com>
2025-03-08 16:08:54 +0530

    Update test.py
    change to us regien

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 from core.binance_api import binance
 import ccxt
+from config.config import API_KEY, API_SECRET
  
 try:
-    market_data = binance.fetch_ticker("BTC/USDT")
-    print("✅ Market Data:", market_data)
+client = Client(API_KEY, API_SECRET, tld="us")
+
+balance = client.get_asset_balance(asset="USDT")
+print(f"💰 USDT Balance: {balance['free']}")
 except ccxt.NetworkError as e:
         print(f"⚠️ Network error: {e}")
 except ccxt.ExchangeError as e:
